221c220c161a0639098be7a90b46411442472922
ho-tonym <ho.tonym@gmail.com>
2019-01-08 15:15:20 -0500

    Updates

---
+++ precache-manifest.0fa7be0cd081ef413e03d23e69cde0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ec24b1384927d9acb8ced2d81e2e6b",
+    "url": "/pokedex/static/media/searchicon.a6ec24b1.svg"
+  },
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "e054046294ef48594cf0",
+    "url": "/pokedex/static/js/main.e0540462.chunk.js"
+  },
+  {
+    "revision": "8fe1c6a061a2f9136c7b",
+    "url": "/pokedex/static/js/1.8fe1c6a0.chunk.js"
+  },
+  {
+    "revision": "e054046294ef48594cf0",
+    "url": "/pokedex/static/css/main.32a2f049.chunk.css"
+  },
+  {
+    "revision": "66d857cd9d7335c7de1a75f502a8908c",
+    "url": "/pokedex/index.html"
+  }
+];
