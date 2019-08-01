3b46d7eb34ed0ed285d5a69f0c4376e985db200a
ho-tonym <ho.tonym@gmail.com>
2019-07-31 21:57:08 -0400

    redux dev tools error

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
 import allReducers from '../reducers/index';
 
 const store = createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)));
