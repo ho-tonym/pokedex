6079cb2f5d8508dafeae47a19bc97024be1a046c
ho-tonym <ho.tonym@gmail.com>
2019-08-06 12:23:11 -0400

    herokj

--- routes/api/mypokemon.js
+++ routes/api/mypokemon.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
 
+const router = express.Router();
 const Pokemon = require('../../models/Pokemon');
 
 // GET api/mypokemon - Get All Pokemon
--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const cors = require('cors');
 const path = require('path')
 
 const app = express();
+const mongoURI = process.env.MONGODB_URI || ('./config/keys').mongoURI
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/mypokemon', require('./routes/api/mypokemon'));// unless hit backend routes, we will hit the index.html
 
-mongoose.connect(process.env.MONGODB_URI || require('./config/keys').mongoURI, { useNewUrlParser: true });
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 // pushto heroku, then have the it build there.
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
@@ -22,5 +24,4 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}!`));// eslint-disable-line no-console
