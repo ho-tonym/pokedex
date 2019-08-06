5e5eb4063951dd9a914e05add6574c0c7ac0c772
ho-tonym <ho.tonym@gmail.com>
2019-08-06 12:03:16 -0400

    heroku 3

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api/mypokemon', require('./routes/api/mypokemon'));
+app.use('/api/mypokemon', require('./routes/api/mypokemon'));// unless hit backend routes, we will hit the index.html
 
 mongoose.connect(process.env.MONGODB_URI || require('./config/keys').mongoURI, { useNewUrlParser: true });
-
-// app.get('/', (req, res) => {
-//   res.send('You have reached the backend server. Wooh!');
-// });
-
+// pushto heroku, then have the it build there.
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));// load index html in client build
   app.get('*', (req, res) => {
   // any requests that arent app.use('/api/mypokemon', require('./routes/api/mypokemon'));, loads index.html
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.hmtl'))// load index html in client/build/index
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))// load index html in client/build/index
   });
 }
 
