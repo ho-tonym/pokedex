741e7b32a7c36003e59951bb63fa3b2b9b6f8221
ho-tonym <ho.tonym@gmail.com>
2019-08-06 14:48:36 -0400

    heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/mypokemon', require('./routes/api/mypokemon'));
 
 mongoose.connect(mongoURI, { useNewUrlParser: true })
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // app.use(express.static('client/build'))
   app.use(express.static(path.join(__dirname, "client", "build")))
   // app.get('*', (req, res) => {
@@ -24,6 +24,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-}
+// }
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));// eslint-disable-line no-console
