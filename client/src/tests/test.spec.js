c2d29d2c9f4b3c15f34add00d2e1e0b23d0f0b67
ho-tonym <ho.tonym@gmail.com>
2019-08-06 13:32:47 -0400

    heroku

--- client/src/tests/test.spec.js
+++ client/src/tests/test.spec.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
-import App from '../App';
 import { create } from "react-test-renderer"
 
 function Button(props) {
--- server.js
+++ server.js
@@ -4,24 +4,22 @@ const cors = require('cors');
 const path = require('path')
 
 const app = express();
-const mongoURI = process.env.MONGODB_URI || ('./config/keys').mongoURI
+
+const mongoURI = process.env.MONGODB_URI || require('./config/keys').mongoURI
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api/mypokemon', require('./routes/api/mypokemon'));// unless hit backend routes, we will hit the index.html
+app.use('/api/mypokemon', require('./routes/api/mypokemon'));
 
-mongoose.connect(mongoURI, { useNewUrlParser: true });
-// pushto heroku, then have the it build there.
-// Serve static assets if in production
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+
 if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('/client/build'));// load index html in client build
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-  // any requests that arent app.use('/api/mypokemon', require('./routes/api/mypokemon'));, loads index.html
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))// load index html in client/build/index
-  });
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));// eslint-disable-line no-console
