5506450f71be23816f49e1a9c978f7db808dd543
ho-tonym <ho.tonym@gmail.com>
2019-08-06 15:42:21 -0400

    done

--- server.js
+++ server.js
@@ -16,14 +16,18 @@ app.use('/api/mypokemon', require('./routes/api/mypokemon'));
 mongoose.connect(mongoURI, { useNewUrlParser: true })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('./client/build'))
   // app.use(express.static(path.join(__dirname, "client", "build")))
   // app.get('*', (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   // })
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/public/index.html'), (err) => {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 }
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));// eslint-disable-line no-console
