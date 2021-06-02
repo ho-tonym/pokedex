a4d01d54370113e06d1d11d822971d53b3153a25
ho-tonym <ho.tonym@gmail.com>
2021-06-02 17:08:51 -0400

    revisit faq

--- client/src/components/Faq/index.js
+++ client/src/components/Faq/index.js
@@ -25,7 +25,7 @@ class Faq extends Component {
           <span />
         </button>
         <div className={`content ${collapseDivs["collapse-1"] ? "display-block" : ""}`}>
-          <p>Pokemon data populated from pokeapi.co.</p>
+          <p>On loadup, we query for API for 20 pokemon. You can scroll down to load 20 more. In search located in the navbar, you can find a specific pokemon that you want more information on. Click on that pokemon to make a request to the API for more information. There is a bot navbar that contains the information of the pokemon you clicked on. If you click on it, it expands to show more information on the type weaknesses/strengths.</p>
         </div>
         <button
           id="collapse-2"
@@ -36,7 +36,9 @@ class Faq extends Component {
           How To Use: My Pokemon
         </button>
         <div className={`content ${collapseDivs["collapse-2"] ? "display-block" : ""}`}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>Here, we can add pokemon and create a list of your pokemon by adding a name and combat power. If no pokemon match the name entered, we will make another request to the api to get the rest of the pokemon. The CP has to be 0 &larr; CP &larr; 9999.</p>
+            <p>It woul d be pretty inconvenient to have to add these pokemon every time to use the app, so we can save the pokemon to the mongo server hosted on MongoDB Atlas. When it is finished saving to the DB, it will return a key that will be can be used to access the all the pokemon saved at a later time in the Get tab. I opted for a key based approach rather than user/pw because it is more convenient.</p>
+            <p>Enter a key here to retrieve data from the backend server and load up previously saved pokemon.</p>
         </div>
         <button
           id="collapse-3"
@@ -44,10 +46,15 @@ class Faq extends Component {
           className="collapsible"
           onClick={this.handleonClick}
         >
-          Nerd Stuff - How it the website works
+          Built With:
         </button>
         <div className={`content ${collapseDivs["collapse-3"] ? "display-block" : ""}`}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>React</p>
+          <p>MongoDB/Mongoose</p>
+          <p>Express.js</p>
+          <p>Node.js</p>
+          <p>Redux</p>
+          <p>Heroku</p>
         </div>
 
       </div>
